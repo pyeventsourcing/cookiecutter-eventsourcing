e9e3ccd335d4d7da8f863bfff902cc8c58a1a45f
John Bywater <john.bywater@appropriatesoftware.net>
2022-02-11 06:42:56 +0000

    Added test to check eventsourcing package can be imported.

--- {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -4,3 +4,9 @@ import {{cookiecutter.project_slug}}
 
 def test_{{cookiecutter.project_slug}}() -> None:
     assert {{cookiecutter.project_slug}}
+
+
+def test_import_eventsourcing() -> None:
+    import eventsourcing
+
+    assert eventsourcing.__version__
